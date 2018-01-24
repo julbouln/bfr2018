f9b0dcf1a5bfd35f4aabd262da9d2889ca55ebba
julbouln <jboulnois@gmail.com>
2018-01-24 18:57:26 +0100

    missing Config.hpp

---
+++ Config.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#define VERSION "0.8.99"
+
+//#define SOUND_SYSTEM_DEBUG
+//#define TRANSITIONS_DEBUG
+//#define TECH_TREE_DEBUG
+//#define FACTORY_DEBUG
+//#define GAME_ENGINE_DEBUG
+//#define COMBAT_DEBUG
+//#define VICTORY_DEBUG
+//#define AI_DEBUG
+//#define BUG_DEBUG
+
+#define ZOOMLEVEL_ENABLE
